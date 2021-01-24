76bdd5b9229edaeffbad3faec1de26212def8ebd
dhenwood <dhenwood@users.noreply.github.com>
2021-01-24 17:10:35 +1100

    Update EncryptionKey.swift
    Made EncryptionKey class public

--- Source/Message/EncryptionKey.swift
+++ Source/Message/EncryptionKey.swift
@@ -18,7 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-class EncryptionKey {
+public class EncryptionKey {
     
     let convUrl: String
     var spaceUserIds: [String] = []
